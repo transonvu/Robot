443db77151bc472eabab17f2ab8a8e857f8b2fdb
transonvu <transonvu96@gmail.com>
2017-11-01 03:13:25 +0700

    Change folder

---
+++ speech/Serial.py
@@ -0,0 +1,29 @@
+import commands
+import serial
+import socket
+
+class Serial:
+    def __init__(self):
+        self.__waitForSerial()
+
+    def __waitForSerial(self):
+        while (1):
+            result = commands.getstatusoutput('ls /dev/ttyUSB*')
+            if (result[0] == 0):
+                self.__serial = serial.Serial(result[1], 9600, timeout=0.050)
+                break
+
+            result = commands.getstatusoutput('ls /dev/ttyACM*')
+            if (result[0] == 0):
+                self.__serial = serial.Serial(result[1], 9600, timeout=0.050)
+                break
+
+    def sendMessage(self, msg):
+        self.__serial.write(msg + "\r\n")
+
+    def readMessage(self):
+        return self.__serial.readline()
+
+    def inWaiting(self):
+	return self.__serial.in_waiting
+
